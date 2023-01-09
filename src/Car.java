bf3db870f029c20103e8be511e19fc0ab6e8f645
Саша <kresh_sasha@icloud.com>
2023-01-09 19:16:56 +0700

    Task 3 Cars

--- src/Car.java
+++ src/Car.java
@@ -1,29 +1,53 @@
 
 public class Car {
-    private String brand;
-    private String model;
-    private double engineVolume;
-    private String color;
-    private int year;
-    private String country;
+    public String brand;
+    public String model;
+    public double engineVolume;
+    public String color;
+    public int year;
+    public String country;
 
-    Car(String brand,String model,double engineVolume,String color,int year,String country) {
+    public Car(String brand,String model,double engineVolume,String color,int year,String country) {
         this.brand = brand;
         this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.year = year;
+        if (engineVolume <= 0) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+        if (color == null || color.isEmpty()) {
+            this.color = "Белый";
+        }else{
+            this.color = color;
+        }
+
+        if (year <= 0) {
+            this.year = 2000;
+        } else {
+            this.year = year;
+        }
         this.country = country;
     }
+
+
     @Override
     public String toString() {
-        return "Car{" +
-                "Бренд'" + brand + '\'' +
-                ", модель'" + model + '\'' +
-                ", объем двигателя = " + engineVolume +
-                ", цвет'" + color + '\'' +
-                ", год выпуска = " + year +
-                ", страна выпуска '" + country + '\'' +
-                '}';
+        if (brand == null || brand.isEmpty()) {
+            return "default";
+        } else if (model == null || model.isEmpty()) {
+            return "default";
+        } else if (country == null || country.isEmpty()) {
+            return "default";
+        } else {
+            return "Car{" +
+                    "Бренд '" + brand + '\'' +
+                    ", модель '" + model + '\'' +
+                    ", объем двигателя = " + engineVolume +
+                    ", цвет '" + color + '\'' +
+                    ", год выпуска = " + year +
+                    ", страна выпуска '" + country + '\'' +
+                    '}';
+        }
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Car cars = new Car("Lada", "Granta", 1.7, "желтого цвета", 2015, "Россия");
-        Car cars1 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "Черный", 2020, "Германия");
-        Car cars2 = new Car("BMW", "Z8",3.0,"Черный",2021,"Германия");
-        Car cars3 = new Car("KIA", "Sportage 4-го поколения",2.4,"Красный",2018,"Южная Корея ");
-        Car cars4 = new Car("Hyundai","Avante",1.6,"Оранжевый",2016,"Южная Корея");
+        Car lada = new Car("Lada", "Granta", 0, "желтого цвета", 2015, "Россия");
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "Черный", 2020, "Германия");
+        Car bmw = new Car("BMW", "Z8",3.0,"Черный",2021,"Германия");
+        Car kia = new Car("KIA", "Sportage 4-го поколения",2.4,"Красный",2018,"Южная Корея ");
+        Car hyundai = new Car("Hyundai","Avante",1.6,"Оранжевый",2016,"Южная Корея");
 
-        System.out.println(cars);
-        System.out.println(cars1);
-        System.out.println(cars2);
-        System.out.println(cars3);
-        System.out.println(cars4);
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
+
     }
 }
